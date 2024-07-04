5f0860de2d65737c919601061ccf2224d0407627
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2024-07-04 07:39:41 +0600

    Project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl"></div>
+    </MaxWidthWrapper>
+  );
+}
