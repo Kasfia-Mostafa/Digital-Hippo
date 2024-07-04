77faba071ec61f3af9ff68aa6dcbfdf7d15f0c4e
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2024-07-04 08:28:55 +0600

    Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,76 @@
+import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { Button, buttonVariants } from "@/ui/button";
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
+
+const perks = [
+  {
+    name: "Instant Delivery",
+    Icon: ArrowDownToLine,
+    description:
+      "Get your assets delivered to your email in seconds and download them right away",
+  },
+  {
+    name: "Guaranteed Quality",
+    Icon: CheckCircle,
+    description:
+      "Every assets on our platform is verified by our team to ensure our highest quality standards. Not happy ? We give 30 days refund guarantee",
+  },
+  {
+    name: "For the plants",
+    Icon: Leaf,
+    description:
+      "We've pledge 5% of of sale to the preservation and restoration of the nature",
+  },
+];
 
 export default function Home() {
   return (
-    <MaxWidthWrapper>
-      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl"></div>
-    </MaxWidthWrapper>
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Your marketplace for high quality{" "}
+            <span className="text-blue-600">digital assets</span>
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Welcome to DigitalHippo. Every asset on our platform is verified by
+            our team to ensure our hightest quality standards
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href="/product" className={buttonVariants()}>
+              Browse Trending
+            </Link>
+            <Button variant="ghost">Our quality promise &rarr;</Button>
+          </div>
+        </div>
+        {/* // TODO : List product */}
+      </MaxWidthWrapper>
+
+      <section className="border-t border-gray-200 bg-gray-50">
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:grid-x-6 lg:grid-cols-3 lg:grid-x-8 lg:gap-y-0">
+            {perks.map((perk) => (
+              <div
+                key={perk.name}
+                className="text-center md:flex md:items-start md:text-left lg:block lg:text-center"
+              >
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="size-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                    {<perk.Icon className="size-1/3"></perk.Icon>}
+                  </div>
+                </div>
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                  <h3 className="text-base font-medium">{perk.name}</h3>
+                  <p className="mt-3 text-sm text-muted-foreground">
+                    {perk.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </>
   );
 }
